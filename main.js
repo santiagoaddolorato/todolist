00409694659900b222d90f80e4c52f72bf557eb4
Santiago Addolorato <addoloratosantiago@gmail.com>
2023-02-06 22:31:03 -0300

    toDoList

---
+++ main.js
@@ -0,0 +1,86 @@
+ 
+ 
+ 
+const input = document.querySelector(".input-text");
+const addBtn = document.querySelector(".add-btn");
+const addForm = document.querySelector(".add-form");
+const tasksList = document.querySelector(".tasks-list");
+const deleteBtn = document.querySelector(".deleteAll-btn");
+
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+ 
+const saveLocalStorage = (tasksList) => {
+  localStorage.setItem("tasks", JSON.stringify(tasksList));
+};
+
+const createTask = (task) =>
+  `<li>${task.name}<img class="delete-btn" src="./img/delete.svg" 
+  alt="boton de borrar" data-name=${task.name}></li>`;
+
+
+
+const renderTasksList = (todoList) => {
+  tasksList.innerHTML = todoList.map((task) => createTask(task)).join("");
+};
+
+ 
+const hideDeleteAll = (tasksList) => {
+  if (!tasksList.length) {
+    deleteBtn.classList.add("hidden");
+    return;
+  }
+  deleteBtn.classList.remove("hidden");
+};
+ 
+const addTask = (e) => {
+  e.preventDefault();
+  const taskName = input.value.trim().replace(/\s+/g, " ");
+  console.log(taskName);
+  if (!taskName.length) {
+    alert("Por favor, ingrese una tarea");
+    return;
+  } else if (
+    tasks.some((task) => task.name.toLowerCase() === taskName.toLowerCase())
+  ) {
+    alert("Ya existe una tarea con ese nombre");
+    return;
+  }
+
+  tasks = [...tasks, { name: taskName }];
+  input.value = "";
+  renderTasksList(tasks);
+  saveLocalStorage(tasks);
+  hideDeleteAll(tasks);
+};
+
+const removeTask = (e) => {
+  if (!e.target.classList.contains("delete-btn")) return;
+
+  const filterName = e.target.dataset.name;
+  console.log(filterName);
+
+  tasks = tasks.filter((task) => task.name !== filterName);
+  renderTasksList(tasks);
+  saveLocalStorage(tasks);
+  hideDeleteAll(tasks);
+};
+
+ 
+const removeAll = () => {
+  tasks = [];
+  renderTasksList(tasks);
+  saveLocalStorage(tasks);
+  hideDeleteAll(tasks);
+};
+
+const init = () => {
+  renderTasksList(tasks);
+  addForm.addEventListener("submit", addTask);
+  tasksList.addEventListener("click", removeTask);
+  deleteBtn.addEventListener("click", removeAll);
+  hideDeleteAll(tasks);
+};
+
+init();
